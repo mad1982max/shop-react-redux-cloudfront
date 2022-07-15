a125da8757e67d55edec7c9cae703a2b40f57d80
Maksym Bezrodnyi <maks.bezrodnyi@gmail.com>
2022-07-15 22:59:26 +0300

    grtById added api

--- src/services/productsAPI.ts
+++ src/services/productsAPI.ts
@@ -2,7 +2,6 @@ import axios from "axios";
 import API_PATHS from "../constants/apiPaths";
 
 export const productsAPI = {
-  getAll: async () => {
-    return await axios.get(`${API_PATHS.bff}/products`);
-  },
+  getAll: async () => await axios.get(`${API_PATHS.main}/products`),
+  getById: async (id: string) => await axios.get(`${API_PATHS.main}/products/${id}`),
 };
