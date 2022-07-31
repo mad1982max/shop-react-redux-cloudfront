1e5f8d1821039ca8203ae20565c4eaf508058baf
Maksym Bezrodnyi <maks.bezrodnyi@gmail.com>
2022-07-31 23:01:58 +0300

    fixes

--- src/constants/apiPaths.ts
+++ src/constants/apiPaths.ts
@@ -1,7 +1,7 @@
 const API_PATHS = {
   product: "https://.execute-api.eu-west-1.amazonaws.com/dev",
   order: "https://.execute-api.eu-west-1.amazonaws.com/dev",
-  import: "https://.execute-api.eu-west-1.amazonaws.com/dev",
+  import: "https://dr6uhn9oqh.execute-api.us-east-1.amazonaws.com/import",
   bff: "https://.execute-api.eu-west-1.amazonaws.com/dev",
   cart: "https://.execute-api.eu-west-1.amazonaws.com/dev",
   main: "https://ui834rx8q3.execute-api.us-east-1.amazonaws.com",
